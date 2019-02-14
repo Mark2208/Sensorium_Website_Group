6cd29a0b1d34951d3b48c49abc32028ea4041118
Mark_Pereira <markpereira2208@gmail.com>
2019-02-14 13:59:26 +0000

    Published Testing Files
    tester.cpp, CMakeLists.txt

---
+++ tester.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+
+
+std::string jay()
+{
+	return "Jay was here\n\n";
+}
+
+std::string mark()
+{
+	return "Mark was here\n\n";
+}
+
+std::string alex()
+{
+	return "Alex was here\n\n";
+}
+
+std::string cian()
+{
+	return "Cian was here\n\n";
+}
+
+std::string aiden()
+{
+	return "Aiden was here\n\n";
+}
+
+std::string kyle()
+{
+	return "Kyle was here\n\n";
+}
+
+int main()
+{
+  //Uncomment your name
+  
+  //std::cout << aiden();
+  //std::cout << alex();
+  //std::cout << cian();
+  //std::cout << jay();
+  //std::cout << kyle();
+  std::cout << mark();
+
+}
+
+
+
+
+
